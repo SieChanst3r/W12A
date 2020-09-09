509e1e3009dd30b9b76374e83b11073f75ea0569
SieChan <sie.chan@outlook.com>
2020-09-08 19:35:29 -0600

    added skeleton for assignment

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//can change the name of the class if needed
+class BoredApi {
+    //filled when the ajax call is finished
+    content = "";
+
+    // function for calling the API
+    getActivity() {
+        //ajax code to call the API and change the screen
+    }
+
+    //bonus stuff
+
+    getGroupActivity() {
+        //ajax code for group activities to the API
+    }
+
+    getFreeActivity() {
+        //ajax code for free activities to the API
+
+    }
+
+    getRecreationalActivity() {
+        //ajax code for recreational activities to the API
+    }
+}
+
+let bored = new BoredApi();
+
+//this needs to be a button that generated an activity
+bored.getActivity();
